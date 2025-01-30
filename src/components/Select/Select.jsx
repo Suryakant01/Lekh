64cbf86c562d4003ed3ba0586b93245e0b3c8633
Suryakant01 <suryakant160502@gmail.com>
2025-01-30 22:57:31 +0530

    fixed active inactive button

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -11,7 +11,7 @@ const Select = ({
 
   return (
     <div className='w-full'>
-      {label && <label htmlFor={id}></label>}
+      {label && <label htmlFor={id} className=''></label>}
 
       <select
         id={id}
@@ -21,7 +21,7 @@ const Select = ({
         {
           options?.map((option) => (
             <option key={option} value={option}>
-              {options}
+              {option}
             </option>            
           ))
         }
